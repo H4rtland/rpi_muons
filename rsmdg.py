f4ba6140977665bce46114ef4996568831b1efe1
H4rtland <h4rtland@gmail.com>
2016-11-04 21:10:47 +0000

    bootstrap navbar working

--- rsmdg.py
+++ rsmdg.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from flask import render_template
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup
+
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+nav = Nav()
+nav.init_app(app)
 
+
+@nav.navigation()
+def nav_bar_renderer():
+    items = []
+    items.append(View("Home", "index"))
+    navbar = Navbar("rsmdg", *items)
+    html = navbar.render()
+    html = html.replace("navbar navbar-default", "navbar navbar-inverse navbar-fixed-top")
+    navbar.render = lambda: html
+    return navbar
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
