2db69dac99b89a380baef01b2aeb9a0c797c70b9
H4rtland <h4rtland@gmail.com>
2016-11-16 18:15:26 +0000

    moved navbar generator to navbar.py

---
+++ navbar.py
@@ -0,0 +1,19 @@
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View, Subgroup, Text, Separator
+
+from app import app
+
+nav = Nav()
+nav.init_app(app)
+
+@nav.navigation()
+def nav_bar_renderer():
+    items = []
+    items.append(View("Home", "index"))
+    items.append(View("Detector", "detector.detector_status"))
+    items.append(View("Result", "result.example_result"))
+    navbar = Navbar("rsmdg", *items)
+    html = navbar.render()
+    html = html.replace("navbar navbar-default", "navbar navbar-inverse navbar-fixed-top")
+    navbar.render = lambda: html
+    return navbar
--- rsmdg.py
+++ rsmdg.py
@@ -1,7 +1,5 @@
 from flask import render_template, jsonify, request
 
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View, Subgroup, Text, Separator
 from flask_bootstrap import Bootstrap
 from flask_admin import Admin
 
@@ -9,6 +7,7 @@ import app
 from app import db, app
 
 from analysis import scheduler
+import navbar
 
 # Import blueprints
 from detector.views import detector as detector_views
@@ -30,26 +29,12 @@ app.config.update(
 db.init_app(app)
 
 Bootstrap(app)
-nav = Nav()
-nav.init_app(app)
 admin = Admin(app, name="rsmdg", template_mode="bootstrap3")
 
 # register blueprints
 app.register_blueprint(detector_views)
 app.register_blueprint(result_views)
 
-
-@nav.navigation()
-def nav_bar_renderer():
-    items = []
-    items.append(View("Home", "index"))
-    items.append(View("Detector", "detector.detector_status"))
-    items.append(View("Result", "result.example_result"))
-    navbar = Navbar("rsmdg", *items)
-    html = navbar.render()
-    html = html.replace("navbar navbar-default", "navbar navbar-inverse navbar-fixed-top")
-    navbar.render = lambda: html
-    return navbar
 
 @app.before_first_request
 def before_first_request():
